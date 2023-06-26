ae59363f2dc1c4eaba8eea48494b2af5f78829d5
Roger Pender <rdpender3@gmail.com>
2023-06-26 10:37:50 -0700

    basic working layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Form />
+      <List />
+      <Footer />
     </div>
+  );
+}
+
+function Header() {
+  return <h1>Packing List 🛫</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do we need to pack?</h3>
+    </div>
+  );
+}
+
+function List() {
+  return <div className="list">Packing List</div>;
+}
+
+function Footer() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you have packed X (X%) for your trip</em>
+    </footer>
   );
 }
 
