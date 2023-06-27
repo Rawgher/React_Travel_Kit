b66d5052403106735af193a8d7950106c7f323bb
Roger Pender <rdpender3@gmail.com>
2023-06-27 10:17:57 -0700

    setting form to update state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const initialItems = [
@@ -22,21 +23,40 @@ function Header() {
 }
 
 function Form() {
+  const [item, setItem] = useState('');
+  const [amount, setAmount] = useState(1);
+
+  function handleInputChange(e) {
+    setItem(e.target.value);
+  }
+
+  function handleSelectChange(e) {
+    setAmount(Number(e.target.value));
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!item) return;
+
+    const newItem = { item, amount, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setItem('');
+    setAmount(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do we need to pack?</h3>
-      <select>
+      <select value={amount} onChange={handleSelectChange}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input type="text" placeholder="Item Name..." value={item} onChange={handleInputChange} />
       <button>Add</button>
     </form>
   );
