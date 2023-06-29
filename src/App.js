3723349d09c20ac478b8ef91de83ff565c701439
Roger Pender <rdpender3@gmail.com>
2023-06-29 13:39:10 -0700

    adding functionality to footer

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       <Header />
       <Form onAddItems={handleAddItems} />
       <List items={itemList} onRemoveItem={handleRemoveItem} onItemPacked={handleItemPacked} />
-      <Footer />
+      <Footer items={itemList} />
     </div>
   );
 }
@@ -94,10 +94,26 @@ function Item({ item, onRemoveItem, onItemPacked }) {
   );
 }
 
-function Footer() {
+function Footer({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding items to your packing list</em>
+      </footer>
+    );
+  }
+
+  let numItems = items.length;
+  let numPacked = items.filter((item) => item.packed).length;
+  let percentPacked = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you have packed X (X%) for your trip</em>
+      <em>
+        {percentPacked === 100
+          ? 'You have everything packed. Have a great trip!'
+          : `You have ${numItems} items on your list, and have packed ${numPacked} items so far (${percentPacked}%) for your trip`}
+      </em>
     </footer>
   );
 }
