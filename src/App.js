c38dbbd3e6ff4d9b029c09ee9df09800914a0f5d
Roger Pender <rdpender3@gmail.com>
2023-07-03 11:13:05 -0700

    sorting items using state

--- src/App.js
+++ src/App.js
@@ -71,13 +71,41 @@ function Form({ onAddItems }) {
 }
 
 function List({ items, onRemoveItem, onItemPacked }) {
+  const [sort, setSort] = useState('input');
+
+  function setSortedStatus(e) {
+    setSort(e.target.value);
+  }
+
+  let sortedItems;
+
+  if (sort === 'input') {
+    sortedItems = items;
+  }
+
+  if (sort === 'name') {
+    sortedItems = items.slice().sort((a, b) => a.item.localeCompare(b.item));
+  }
+
+  if (sort === 'packed') {
+    sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item item={item} key={item.id} onRemoveItem={onRemoveItem} onItemPacked={onItemPacked} />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sort} onChange={setSortedStatus}>
+          <option value="input">Sort by input order</option>
+          <option value="name">Sort by name</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
